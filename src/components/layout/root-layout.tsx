8722288893a6976855f7ae9c9da02c3413e7db3e
norriswu0 <norris.wu.au+github@outlook.com>
2025-11-27 13:15:26 +0000

    tweak to accomondate presentation layout

--- src/components/layout/root-layout.tsx
+++ src/components/layout/root-layout.tsx
@@ -20,9 +20,9 @@ type Props = {
 export function RootLayout({ children }: Props) {
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
+      className={`${geistSans.className} ${geistMono.className} flex min-h-dvh items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
-      <main className="flex min-h-screen w-full max-w-4xl flex-col p-8 md:py-32 md:px-16 bg-white dark:bg-black sm:items-start z-10">
+      <main className="flex min-h-dvh max-h-dvh w-full max-w-4xl flex-col p-8 md:py-32 md:px-16 bg-white dark:bg-black sm:items-start z-10 overflow-x-hidden overflow-y-auto">
         <Link href="/">
           <ShinyText text="STORY" speed={3} className="text-3xl" />
         </Link>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,68 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+
+type Dimension = {
+  width: number;
+  height: number;
+};
 
 export default function AboutPage() {
-  return <div>about</div>;
+  const containerRef = useRef<React.ComponentRef<"div">>(null);
+  const [sectionDimension, setSectionDimension] = useState<Dimension>({
+    width: 0,
+    height: 0,
+  });
+
+  useLayoutEffect(() => {
+    const contaienr = containerRef.current;
+    if (!contaienr) return;
+    const updateSectionDimension = () => {
+      setSectionDimension({
+        width: contaienr.offsetWidth,
+        height: contaienr.offsetHeight,
+      });
+    };
+
+    updateSectionDimension();
+  }, []);
+
+  return (
+    <div
+      id="scrolling-container"
+      ref={containerRef}
+      className="flex-1 size-full my-4 overflow-y-auto"
+    >
+      <TheReality dimension={sectionDimension} />
+      <TheProblem dimension={sectionDimension} />
+      <TheIdea dimension={sectionDimension} />
+    </div>
+  );
+}
+
+function TheReality(props: { dimension: Dimension }) {
+  return (
+    <div
+      className="ring-1"
+      style={{ width: props.dimension.width, height: props.dimension.height }}
+    >
+      The Reality
+    </div>
+  );
+}
+function TheProblem(props: { dimension: Dimension }) {
+  return (
+    <div
+      style={{ width: props.dimension.width, height: props.dimension.height }}
+    >
+      The Problem
+    </div>
+  );
+}
+function TheIdea(props: { dimension: Dimension }) {
+  return (
+    <div
+      style={{ width: props.dimension.width, height: props.dimension.height }}
+    >
+      The Idea
+    </div>
+  );
 }
