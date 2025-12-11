82628ba5af68ef916a595ad1ea01e69285751b21
norriswu0 <norris.wu.au+github@outlook.com>
2025-12-11 10:42:42 +0000

    fix api route base url

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,22 @@
 import { ProfilerUI } from "@/module/profiler/component/profiler-ui";
+import { headers } from "next/headers";
 
 export default async function Page() {
+  const headersList = await headers();
+
+  const protocal = headersList.get("x-forwarded-proto");
+  const host = headersList.get("x-forwarded-host");
+
   async function chat(message: string) {
     "use server";
 
-    const res = await fetch("http://localhost:3000/api/profiler", {
+    const apiUrl = `${protocal}://${host}/api/profiler`;
+    console.debug({
+      apiUrl,
+      message,
+    });
+
+    const res = await fetch(apiUrl, {
       method: "POST",
       body: JSON.stringify({
         message,
